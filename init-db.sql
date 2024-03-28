47af4fa5f3d759472d81ec1b0666fff9cdfb8b26
IUH20098661 <144584667+KhaiKhua@users.noreply.github.com>
2024-03-28 15:42:07 +0700

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS tblbookIUHX (
 );
 
 INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+  ('DevOps', '20098661'),
+  ('Big Data', '20098661'),
+  ('Cloud Deployement', '20098661'),
+('Data Analysis', '20098661'),
+('Block Chain', '20098661');
